3f97a0478eea4cc38808cdc9a4775dfd7a41d4a2
Jakub <hoblikj@gmail.com>
2019-11-23 00:24:50 +0100

    import fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getTimeStamp from '../helpers/TimeStampSeconds'
+import getTimeStamp from '@/helpers/TimeStampSeconds'
 import events from './events'
 
 Vue.use(Vuex)

44dba014803fb4bab63f580d60a073f6d0742b99
Jakub <hoblikj@gmail.com>
2019-11-22 01:09:40 +0100

    router-view fade transition

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@
         </nav>
       </div>
     </div>
-    <router-view class="container px-6 mx-auto pt-20"/>
+    <transition name="fade" mode="out-in">
+      <router-view class="container px-6 mx-auto pt-20"/>
+    </transition>
   </div>
 </template>
 
@@ -24,4 +26,15 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
 </style>

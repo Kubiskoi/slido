e83a3222a91a7644ac00335ad140aaf7dbc4c23c
Jakub <hoblikj@gmail.com>
2019-11-22 20:39:12 +0100

    routes in separet file

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Events from '../views/Events.vue'
-import AddEvent from '../views/AddEvent.vue'
-import EditEvent from '../views/EditEvent.vue'
+import routes from './routes'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'events',
-    component: Events
-  },
-  {
-    path: '/add-event',
-    name: 'add-event',
-    component: AddEvent
-  },
-  {
-    path: '/edit-event/:eventId',
-    name: 'edit-event',
-    component: EditEvent
-  }
-]
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+import Events from '../views/Events.vue'
+import AddEvent from '../views/AddEvent.vue'
+import EditEvent from '../views/EditEvent.vue'
+
+export default [
+  {
+    path: '/',
+    name: 'events',
+    component: Events
+  },
+  {
+    path: '/add-event',
+    name: 'add-event',
+    component: AddEvent
+  },
+  {
+    path: '/edit-event/:eventId',
+    name: 'edit-event',
+    component: EditEvent
+  }
+]
